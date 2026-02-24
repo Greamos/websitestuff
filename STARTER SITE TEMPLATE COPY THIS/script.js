b7ad207309747948a7ac927c579ac98a4fa6f2e0
Greamos <nvboxtel310@gmail.com>
2026-02-24 12:58:48 +0100

    finally adding the template folder

---
+++ STARTER SITE TEMPLATE COPY THIS/script.js
@@ -0,0 +1,12 @@
+// Hello World JavaScript Template
+console.log('Hello World! JavaScript is loaded and running.');
+
+// Get button element
+const button = document.getElementById('clickBtn');
+
+// Add click event listener
+button.addEventListener('click', () => {
+    alert('Hello! You clicked the button!');
+    console.log('Button was clicked!');
+});
+
